770b1e45bbd537a8fecb0ea2c812236256d68132
NishanthSpShetty <nishanthspshetty@gmail.com>
2021-06-14 19:24:24 +0530

    get mutable reference to data and modify while cell still holds data :BAD_STUFF

--- pointer/src/cell.rs
+++ pointer/src/cell.rs
@@ -17,11 +17,8 @@ impl<T> Cell<T> {
         };
     }
 
-    pub fn get(&self) -> T
-    where
-        T: Copy,
-    {
-        unsafe { *self.value.get() }
+    pub fn get(&self) -> &mut T {
+        unsafe { &mut *self.value.get() }
     }
 }
 
@@ -36,12 +33,6 @@ mod test {
 
     #[test]
     fn test_i32_cell() {
-        let cell = super::Cell::new(100);
-
-        assert_eq!(cell.get(), 100);
-        cell.set(200);
-        assert_eq!(cell.get(), 200);
-
         let x = std::sync::Arc::new(Cell::new(100));
 
         let x1 = x.clone();
@@ -53,5 +44,18 @@ mod test {
         spawn(move || {
             x2.set(200);
         });
+    }
+
+    #[test]
+    fn test_bad_pointer() {
+        let x = Cell::new(String::from("hello worldmjdjdnbd"));
+        //get the pointer to the data
+        let first = x.get();
+        //while we hold pointer to data, we replace what cell holds it.
+        x.set(String::from(
+            " lorem epssum ujbjhsdbfjhasdb jghkzxcbcvjkahsdb jkhsdb:World",
+        ));
+        first.push_str("pushh");
+        eprintln!("{}", first);
     }
 }
