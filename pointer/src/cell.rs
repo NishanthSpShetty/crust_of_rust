fad0dd00574c32518a63def94a14a06a57181ed3
NishanthSpShetty <nishanthspshetty@gmail.com>
2021-06-14 18:55:01 +0530

    unsafley implement sync and make multiple mutation from threads

--- pointer/src/cell.rs
+++ pointer/src/cell.rs
@@ -25,8 +25,15 @@ impl<T> Cell<T> {
     }
 }
 
+///UnsafeCell is nopt thread safe and cannot be shared across thread boudaries
+///it impl !Sync marker which enforces the struct `Cell` also not sync
+unsafe impl<T> Sync for Cell<T> {}
+
 #[cfg(test)]
 mod test {
+    use super::*;
+    use std::thread::spawn;
+
     #[test]
     fn test_i32_cell() {
         let cell = super::Cell::new(100);
@@ -34,5 +41,17 @@ mod test {
         assert_eq!(cell.get(), 100);
         cell.set(200);
         assert_eq!(cell.get(), 200);
+
+        let x = std::sync::Arc::new(Cell::new(100));
+
+        let x1 = x.clone();
+        spawn(move || {
+            x1.set(200);
+        });
+
+        let x2 = x.clone();
+        spawn(move || {
+            x2.set(200);
+        });
     }
 }
